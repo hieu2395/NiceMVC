26137299123142b73b0b7898244b5031770c32de
TUONG <nmtuong1995@gmail.com>
2017-01-12 19:10:21 +0700

    ABC

--- WebAimar/WebAimar/Properties/AssemblyInfo.cs
+++ WebAimar/WebAimar/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2d7c23f5-e9c8-46a1-91fe-548be3b13ad4")]
+[assembly: Guid("6df34cdd-8312-47a6-9e88-d5ec8f202fc6")]
 
 // Version information for an assembly consists of the following four values:
 //
